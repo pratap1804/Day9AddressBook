899de307fc5766ecdf2d41d7fadd5f145ca0ad99
pratap1804 <pratap.1804@gmail.com>
2021-06-29 12:59:18 +0530

    Added: Edit Contact Method to edit people details using its E-Mail

--- AddressBook/src/Book.java
+++ AddressBook/src/Book.java
@@ -12,49 +12,12 @@ public class Book {
 
         System.out.println("Welcome to the address book program");
 
-        getAddContact();
+        Methods.getAddContact();
+
+        Methods.editContact();
+
 
     }
 
-    public static People getAddContact(){
-
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter FirstName:");
-        String firstName = sc.nextLine();
-
-        System.out.println("Enter LastName: ");
-        String lastName = sc.nextLine();
-
-        System.out.print("Enter Address: ");
-        String address = sc.nextLine();
-
-        System.out.print("Enter City: ");
-        String city = sc.nextLine();
-
-        System.out.print("Enter State: ");
-        String state = sc.nextLine();
-
-        System.out.print("Enter ZipCode: ");
-        String zip = sc.nextLine();
-
-        System.out.print("Enter Mobile Number: ");
-        String phoneNumber = sc.nextLine();
-
-        System.out.println("Enter Email Id:");
-        String EMail = sc.nextLine();
-
-        People people = new People();
-        people.setFirstName(firstName);
-        people.setLastName(lastName);
-        people.setAddress(address);
-        people.setCity(city);
-        people.setZip(zip);
-        people.setState(state);
-        people.setPhoneNumber(phoneNumber);
-        people.setEMail(EMail);
-        System.out.println(people);
-
-        return people;
-    }
 }
+
